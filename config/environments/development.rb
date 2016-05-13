ecac0d63aea74f3bdaea89d933aa63bc3f912697
hannanzahar <hannanzahar@outlook.com>
2016-05-13 17:59:07 +0800

    basic mailer finished

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,9 +40,9 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # config/environments/{development,test}.rb
-    config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
     
-    config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: ENV['mailer_domain'] }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
